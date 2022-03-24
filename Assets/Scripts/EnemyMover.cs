879f70b37fe6cf1f198e28fb94b63797c6aa0431
Mahmoud0090 <mmh.00090@gmail.com>
2022-03-25 02:56:37 +0300

    Enemy Mover and Creating path

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
+    [SerializeField] float waitTime = 1f;
     void Start()
     {
-        PrintWayPointName();
+        StartCoroutine(FollowPath());
     }
 
-   void PrintWayPointName()
+   IEnumerator FollowPath()
     {
         foreach(WayPoint wayPoint in path)
         {
-            Debug.Log(wayPoint.name);
+            transform.position = wayPoint.transform.position;
+
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
